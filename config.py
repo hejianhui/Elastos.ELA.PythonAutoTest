caef8ae746cc8cced228a9331bfe07a325c90c93
bocheng0000 <bocheng0000@gmail.com>
2018-05-16 19:50:18 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Bocheng.Zhang
+@license: Apache Licence 
+@contact: bocheng0000@gmail.com
+@file: config.py
+@time: 2018/5/9 14:54
+"""
+
+node_name = 'ela'
+config_file = 'config.json.sample'
+# config_file = 'config.json'
+node_src_path = ['src', 'github.com', 'elastos', 'Elastos.ELA']
+# node_src_path = ['src', 'Elastos.ELA']
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
